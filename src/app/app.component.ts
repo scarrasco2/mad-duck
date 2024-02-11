eb83837558f45788a38ba85c8e22eb2af5ccfabe
samuel.carrasco <samuel.carrasco@huskers.unl.edu>
2024-02-11 00:43:46 -0600

    refactor: angular 17 directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { MadDuckCaptureComponent } from './mad-duck-capture.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, MadDuckCaptureComponent],
+  imports: [RouterModule, MadDuckCaptureComponent],
   selector: 'mad-duck-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'mad-duck';
-}
+export class AppComponent {}
--- src/app/mad-duck-capture.component.ts
+++ src/app/mad-duck-capture.component.ts
@@ -2,22 +2,27 @@ import { Component, Input, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ActivatedRoute } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
-  selector: 'mad-duck-mad-duck-capture',
+  selector: 'mad-duck-capture',
   standalone: true,
   imports: [CommonModule, MatProgressSpinnerModule],
   templateUrl: './mad-duck-capture.component.html',
   styleUrl: './mad-duck-capture.component.css',
 })
 export class MadDuckCaptureComponent implements OnInit {
-  @Input() flagName = 'Mad Duck';
+  @Input() flagName = '';
   isLoading = false;
-  private route = inject(ActivatedRoute);
+  route = inject(ActivatedRoute);
+  title = inject(Title);
+
   onClick() {
     this.isLoading = true;
   }
+
   ngOnInit(): void {
     this.flagName = this.route.snapshot.url[0].path;
+    this.title.setTitle('Operation Mad Duck' + ' | ' + this.flagName + ' Flag');
   }
 }
